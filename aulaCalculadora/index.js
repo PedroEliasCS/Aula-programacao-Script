482a001ca43327f1e54cdd8fc816e62823b22e05
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2024-08-19 22:03:54 -0300

    Dia 19/08

--- aulaCalculadora/index.js
+++ aulaCalculadora/index.js
@@ -1,1 +1,57 @@
+const { createApp } = Vue;
 
+const inicial = {
+  display: "0",
+  numeroAtual: "",
+  numerooAnterior: "",
+  operador: null,
+};
+
+createApp({
+  data() {
+    return inicial;
+  },
+  methods: {
+    lidarBotao(botao) {
+      console.log(botao);
+      switch (botao) {
+        case "*":
+          this.lidarOperador(botao);
+          return;
+        case "/":
+          this.lidarOperador(botao);
+          return;
+        case "+":
+          this.lidarOperador(botao);
+          return;
+        case "-":
+          this.lidarOperador(botao);
+          return;
+        case ".":
+          this.lidarDecimal();
+          return;
+        case "=":
+          this.lidarIgual();
+          return;
+        case "AC":
+          this.lidarClean();
+          return;
+        default:
+          this.lidarNumero(botao);
+          return;
+      }
+    },
+
+    lidarClean() {
+      this.display = "Clean";
+      this.numeroAtual = "";
+      this.numerooAnterior = "";
+      this.operador = null;
+    },
+
+    lidarNumero(numero) {
+      this.numeroAtual = this.numeroAtual + numero;
+      this.display = this.numeroAtual;
+    },
+  },
+}).mount("#app");
